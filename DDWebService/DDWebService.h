d8af398feba7a51e2af6f7da565569c4aa662536
shanjunlong <shanjunlong@smartdevices.com.cn>
2018-03-30 11:09:28 +0800

    ref |> 添加支持carthage丢失的头文件

---
+++ DDWebService/DDWebService.h
@@ -0,0 +1,27 @@
+//  DDWebService.h
+//  Created on 2018/3/28
+//  Description <#文件描述#>
+
+//  Copyright © 2018年 Huami inc. All rights reserved.
+//  @author shanjunlong(shanjunlong@huami.com)
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DDWebService.
+FOUNDATION_EXPORT double DDWebServiceVersionNumber;
+
+//! Project version string for DDWebService.
+FOUNDATION_EXPORT const unsigned char DDWebServiceVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDWebService/PublicHeader.h>
+
+#import <DDWebService/WSNetworkTool.h>
+#import <DDWebService/WSRequestTask.h>
+#import <DDWebService/WSTypeDefines.h>
+#import <DDWebService/WSRequestCallbackProtocol.h>
+#import <DDWebService/NSString+WebService.h>
+#import <DDWebService/WSNetworkMonitor.h>
+#import <DDWebService/NSError+WebServie.h>
+#import <DDWebService/WSNetworkConfig.h>
+#import <DDWebService/AFHTTPRequestSerializer+WSHTTPHeader.h>
